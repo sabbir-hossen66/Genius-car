a12f23e7a797261d0a5cb1447c453ef9e6d1d979
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-05-04 11:02:13 +0600

    services component done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
 import About from '../../Components/About/About';
+import Services from '../../Components/Services/Services';
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <About></About>
+      <Services></Services>
     </div>
   );
 };
