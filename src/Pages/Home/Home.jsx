783f5448c83d98a047c6b1c1bfc6a88faa5bd724
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-05-04 10:05:46 +0600

    about us done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
+import About from '../../Components/About/About';
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <About></About>
     </div>
   );
 };
